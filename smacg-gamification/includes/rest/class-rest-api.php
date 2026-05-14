fe0e98190227b203164cda6114b550af9348acc2
smaacg <smaacg@gmail.com>
2026-05-15 05:18:32 +0800

    Delete smacg-gamification directory

--- smacg-gamification/includes/rest/class-rest-api.php
+++
@@ -1,9 +0,0 @@
-add_action( 'rest_api_init', function () {
-    register_rest_route( 'smacg/v1', '/user-level', [
-        'methods'             => 'GET',
-        'permission_callback' => 'is_user_logged_in',
-        'callback'            => fn() => rest_ensure_response(
-            smacg_get_user_level( get_current_user_id() )
-        ),
-    ] );
-} );
